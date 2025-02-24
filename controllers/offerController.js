4b687b58f35f06fe2060574b1f961492caf8980f
Gojsa <gojsaoff@gmail.com>
2025-02-24 20:50:37 +0100

    offer finish all

--- controllers/offerController.js
+++ controllers/offerController.js
@@ -4,20 +4,31 @@ const Offer = db.offer;
 const OfferArticle = db.offerArticle;
 const { Op, QueryTypes, where } = require("sequelize");
 
+const redirectToOffer = asyncHandler(async (req, res) => {
+
+    res.render("offer/allOffer");
+})
 const findAllOffers = asyncHandler(async (req, res) => {
     const search = req.query.search;
-    const limit = req.query.limit;
-    const offset = req.query.offset;
+    const limit = req.params.limit;
+    const offset = req.params.offset;
+    const queryCount = `
+    select count(*) as count from offer
+
+    `;
+    const offersCount = await db.sequelize.query(queryCount, {
+        type: QueryTypes.SELECT,
+    });
     const query = `
     select * from offer
     where
-    first_name like '%${search}%' or
-    last_name like '%${search}%' or 
-    car_name like '%${search}%' or
-    car_plate like '%${search}%' or
+    first_name ilike '%${search}%' or
+    last_name ilike '%${search}%' or 
+    car_name ilike '%${search}%' or
+    car_plate ilike '%${search}%' or
     jib like '%${search}%'
-    limit ${limit} offset ${offset}
     order by created_at desc
+      limit ${limit} offset ${offset - 1}
 
     `;
     const offers = await db.sequelize.query(query, {
@@ -28,7 +39,149 @@ const findAllOffers = asyncHandler(async (req, res) => {
         res.status(404);
         throw new Error("No offers found");
     }
-    res.status(200).json(offers);
+    res.status(200).json({ offers, totalCount: offersCount[0].count });
 })
 
-module.exports = { findAllOffers };
+const createOffer = asyncHandler(async (req, res) => {
+    const {  first_name, last_name, car_name, car_plate, jib, note } = req.body;
+    let number;
+    const findOffer = await Offer.findOne({
+        order: [['created_at', 'DESC']],
+    });
+    const year = new Date().getFullYear().toString().slice(-2); // Get the last two digits of the year
+    if (!findOffer) {
+        number = `0001/${year}`;
+    } else {
+        const lastNumber = findOffer.number.split("/");
+        if (lastNumber[1] == year) {
+            number = `${(parseInt(lastNumber[0]) + 1).toString().padStart(4, '0')}/${year}`;
+        } else {
+            number = `0001/${year}`;
+        }
+    }
+    
+    const offer = await Offer.create({
+        number,
+        first_name,
+        last_name,
+        car_name,
+        car_plate,
+        jib,
+        note,
+    });
+    if (!offer) {
+        res.status(400);
+        throw new Error("Offer not created");
+    }
+    res.status(201).json({ offer });
+})
+const updateOffer = asyncHandler(async (req, res) => {
+    const offer_id = req.params.offer_id;
+    const { number, first_name, last_name, car_name, car_plate, jib, note } = req.body;
+    const offer = await Offer.update({
+        number,
+        first_name,
+        last_name,
+        car_name,
+        car_plate,
+        jib,
+        note,
+    }, {
+        where: {
+            offer_id
+        },
+    });
+    if (!offer) {
+        res.status(400);
+        throw new Error("Offer not updated");
+    }
+    res.status(201).json({ offer });
+})
+const deleteOffer = asyncHandler(async (req, res) => {
+    const offer_id = req.params.offer_id;
+    const offer = await Offer.destroy({
+        where: {
+            offer_id
+        },
+    });
+    if (!offer) {
+        res.status(400);
+        throw new Error("Offer not deleted");
+    }
+    res.status(201).json({ offer });
+})
+
+const findAllOfferArticles = asyncHandler(async (req, res) => {
+    const offer_id = req.params.offer_id;
+    const offerArticles = await OfferArticle.findAll({
+        where: {
+            offer_id
+        },
+    });
+    if (!offerArticles) {
+        res.status(404);
+        throw new Error("No offer articles found");
+    }
+    res.render("offer/offerArticle", { offerArticles });
+});
+const createOfferArticle = asyncHandler(async (req, res) => {
+    const { offer_id, article_name, amount, price_with_vat, note } = req.body;
+    const offerArticle = await OfferArticle.create({
+        offer_id, article_name, amount, price_with_vat, note
+    });
+    if (!offerArticle) {
+        res.status(400);
+        throw new Error("Offer article not created");
+    }
+    res.status(201).json({ offerArticle });
+})
+const updateOfferArticle = asyncHandler(async (req, res) => {
+    const offer_article_id = req.params.offer_article_id;
+    const { article_name, amount, price_with_vat, note } = req.body;
+    const offerArticle = await OfferArticle.update({
+        article_name, amount, price_with_vat, note
+    }, {
+        where: {
+            offer_article_id
+        },
+    });
+    if (!offerArticle) {
+        res.status(400);
+        throw new Error("Offer article not updated");
+    }
+    res.status(201).json({ offerArticle });
+})
+
+const deleteOfferArticle = asyncHandler(async (req, res) => {
+    const offer_article_id = req.params.offer_article_id;
+    const offerArticle = await OfferArticle.destroy({
+        where: {
+            offer_article_id
+        },
+    });
+    if (!offerArticle) {
+        res.status(400);
+        throw new Error("Offer article not deleted");
+    }
+    res.status(201).json({ offerArticle });
+})
+
+const renderPrintOffer = asyncHandler(async (req, res) => {
+    const offer_id = req.params.offer_id;
+    const offer = await Offer.findOne({
+        where: {
+            offer_id
+        },
+    });
+    const offerArticles = await OfferArticle.findAll({
+        where: {
+            offer_id
+        },
+    });
+    if (!offer || !offerArticles) {
+        res.status(404);
+        throw new Error("No offer or offer articles found");
+    }
+    res.render("offer/printOffer", { offer, offerArticles });
+});
+module.exports = {renderPrintOffer,deleteOfferArticle, updateOfferArticle, createOfferArticle, findAllOfferArticles, deleteOffer, updateOffer, redirectToOffer, createOffer, findAllOffers };
--- routes/offerRoute.js
+++ routes/offerRoute.js
@@ -2,11 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    findAllOffers
+    findAllOffers,
+    createOffer,
+    redirectToOffer,
+    updateOffer,
+    deleteOffer,
+    findAllOfferArticles,
+    createOfferArticle,
+    updateOfferArticle,
+    deleteOfferArticle,
+    renderPrintOffer
 } = require("../controllers/offerController");
 
-
-router.get("/redirect-to-all", findAllOffers);
+router.get("/offer-article-one-offer/:offer_id", findAllOfferArticles);
+router.get("/print/:offer_id", renderPrintOffer);
+router.get("/redirect-to-all", redirectToOffer);
+router.get("/all-offer/:limit/:offset", findAllOffers);
+router.post("/", createOffer);
+router.post("/article", createOfferArticle);
+router.put("/:offer_id", updateOffer);
+router.put("/article/:offer_article_id", updateOfferArticle);
+router.delete("/:offer_id", deleteOffer);
+router.delete("/article/:offer_article_id", deleteOfferArticle);
 
 
 module.exports = router;
